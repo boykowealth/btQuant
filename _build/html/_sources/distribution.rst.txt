9aa27d06bbb042e0b913e10dd863c78fa645be08
boykowealth <braydenboyko@boykowealth.com>
2026-02-10 22:30:11 -0700

    Options Documentation Updated - v1.0.2

--- _build/html/_sources/distribution.rst.txt
+++
@@ -1,7 +0,0 @@
-distributions module
-===================
-
-.. automodule:: btQuant.distributions
-    :members:
-    :undoc-members:
-    :show-inheritance:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))  # Add project root to sys.path
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
